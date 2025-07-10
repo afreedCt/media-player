5c9a5ea2fd2ce86e8eadb9122c2cee5586c2a15f
shanifkcsha <shanifkc60@gmail.com>
2025-07-10 14:25:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Landing from './pages/Landing'
+import Home from './pages/Home'
+import History from './pages/History'
+
+
+function App() {
+
+  return (
+    <>
+    <Landing />
+    <Home />
+    <History />
+    </>
+  )
+}
+
+export default App
